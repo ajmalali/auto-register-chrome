8b2e79bbe769a15704c716d392bfb6b970e0fa33
Ajmal Ali <ajmalsyedali@gmail.com>
2018-07-21 00:30:58 +0530

    Update browser action to open/switch options.html

--- js/background.js
+++ js/background.js
@@ -1,6 +1,23 @@
-// Open options page when extension icon is clicked
-// add checks so that it opens only once
+// Open/switch to options page when extension icon is clicked
 chrome.browserAction.onClicked.addListener(function () {
    let optionsURL = chrome.extension.getURL('options.html');
-   chrome.tabs.create({url: optionsURL});
+   let isInWindow = false;
+   // Get all tabs in current window
+   chrome.tabs.query({}, function (tabs) {
+      tabs.forEach(function (tab) {
+         // Check if options page is present in the window
+         if(tab.url === optionsURL) {
+            isInWindow = true;
+            // if present, check if active (tab selected)
+            if(!tab.active) {
+               // Set options page to active
+               chrome.tabs.update(tab.id, {active: true});
+            }
+         }
+      });
+      // Create new tab if not present
+      if(!isInWindow) {
+          chrome.tabs.create({url: optionsURL});
+      }
+   });
 });
