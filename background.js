3b520b5a7e9fbaa1d15d44540262bc5702f5efd0
Ajmal Ali <ajmalsyedali@gmail.com>
2018-07-20 14:48:16 +0530

    Move background.js and options.js to new js folder

--- background.js
+++
@@ -1,6 +0,0 @@
-// Open options page when extension icon is clicked
-// add checks so that it opens only once
-chrome.browserAction.onClicked.addListener(function () {
-   let optionsURL = chrome.extension.getURL('options.html');
-   chrome.tabs.create({url: optionsURL});
-});
